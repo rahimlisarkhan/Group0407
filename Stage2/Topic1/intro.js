7b1d17451bc26394fc0b6a7d37a82b26f9d06bc9
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-06-10 00:45:39 +0400

    upt

---
+++ Stage2/Topic1/intro.js
@@ -0,0 +1,48 @@
+// console.log('Hello world');
+
+// ====== PRIMITiVE ====== (STACK) 5
+
+// NUMBER
+var age = 5;
+
+var accept = 15;
+
+var c = age + accept;
+
+var d = age + 25;
+
+var num1 = 20; // Integer
+var num2 = 20.5; // Float
+
+// console.log(c);
+// console.log(d);
+
+//STRING
+var fullName = 'John Doe';
+var address = `John
+dsdsdsadsadasdsadasdas
+dasdasdsadsadadsadsa
+Doe`;
+
+// console.log(address);
+
+//BOOLEAN
+var isNew = false;
+var isMarried = true;
+
+// NULL
+var imgUrl = 'https://lorempixel.com/images/flower.png';
+var imgUrl2 = null;
+
+//Undefined
+// var age5 = undefined;
+
+var lesson;
+
+lesson = 10;
+
+console.log(lesson);
+
+lesson = 20;
+
+console.log(lesson);
---
+++ Stage2/Topic1/operators.js
@@ -0,0 +1,76 @@
+// JavaScript Arithmetic Operators =======
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+// ( )	Group
+
+// var x = 2;
+// var y = 10;
+
+// JavaScript Assignment Operators =======
+// y = y - 2;
+// y -= 2;
+// y += 2;
+// y *= 2;
+
+// var z = x;
+// var z = 4;
+
+// z = z + y;
+// z = z - y;
+
+// console.log(z); // 14
+
+// var result = x + y; // 12
+// var result = y - x; // 2
+// var result = y * x; // 20
+// var result = y / x; // 5
+// var result = y ** x; // 100
+// var result = y % x; // 0
+// var result = y + x * 2; // 14
+// var result = (y + x) * 2; // 24
+
+// y++;
+// y--;
+
+// y = y + 1;
+// y = y - 1; // 9
+// y = y * 1;
+
+// console.log(y);
+
+// JavaScript Comparison
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// <=
+// >=
+
+// var a = 10;
+// var b = 10;
+
+// var result = a == b;
+// var result = a === b;
+// var result = a < b;
+// var result = a > b;
+// var result = a >= b;
+// var result = a <= b;
+
+// Logical Assignment Operators
+var age = 17;
+var citizen = 'az';
+var weight = 75;
+
+// var result = age >= 18 && citizen == 'az' && weight <= 75;
+var result = citizen == 'az' || weight <= 75;
+
+console.log(result);
