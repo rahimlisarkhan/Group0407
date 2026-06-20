311edc71a9e1749f4a7bc301729d6b8bf41b6b07
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-06-20 17:15:26 +0400

    upt

---
+++ Stage2/Topic1/switch.js
@@ -0,0 +1,117 @@
+// var userMonth = prompt('Please enter month number'); // Jan
+// var monthIndex = userMonth - 1; // Jan
+
+// if (monthIndex == 0) {
+//   console.log('Jan');
+// } else if (monthIndex == 1) {
+//   console.log('Feb');
+// } else if (monthIndex == 2) {
+//   console.log('Mar');
+// } else if (monthIndex == 3) {
+//   console.log('Apr');
+// } else if (monthIndex == 4) {
+//   console.log('May');
+// } else if (monthIndex == 5) {
+//   console.log('Jun');
+// } else if (monthIndex == 6) {
+//   console.log('Jul');
+// } else if (monthIndex == 7) {
+//   console.log('Aug');
+// } else if (monthIndex == 8) {
+//   console.log('Sep');
+// } else if (monthIndex == 9) {
+//   console.log('Oct');
+// } else if (monthIndex == 10) {
+//   console.log('Nov');
+// } else if (monthIndex == 11) {
+//   console.log('Dec');
+// } else {
+//   console.log('Incorrect month index');
+// }
+
+// switch (monthIndex) {
+//   case 0:
+//     console.log('Jan');
+//     break;
+//   case 1:
+//     console.log('Feb');
+//     break;
+//   case 2:
+//     console.log('Mar');
+//     break;
+//   case 3:
+//     console.log('Apr');
+//     break;
+//   case 4:
+//     console.log('May');
+//     break;
+//   case 5:
+//     console.log('Jun');
+//     break;
+//   case 6:
+//     console.log('Jul');
+//     break;
+//   case 7:
+//     console.log('Aug');
+//     break;
+//   case 8:
+//     console.log('Sep');
+//     break;
+//   case 9:
+//     console.log('Oct');
+//     break;
+//   case 10:
+//     console.log('Nov');
+//     break;
+//   case 11:
+//     console.log('Dec');
+//     break;
+//   default:
+//     console.log('Incorrect month index');
+// }
+
+// var a = 'H';
+// var a = 'H';
+// var b = 2;
+
+// console.log(a + b); // Not a Number - NaN
+
+// var numA = parseInt(a);
+
+// var result = isNaN(a); //true
+
+// console.log(result);
+
+// if (result) {
+//   console.log('Sehv info');
+// } else {
+//   console.log(parseInt(a) + b);
+// }
+
+// if (!result) {
+// if (result == false) {
+//   console.log(parseInt(a) + b);
+// }
+
+var age = 14;
+
+// var name;
+
+// BASIC
+// if (age >= 18) {
+//   name = 'cenab';
+// } else {
+//   name = 'balaca';
+// }
+
+// var name = 'balaca';
+
+// if (age >= 18) {
+//   name = 'cenab';
+// }
+
+//PRO | Ternary
+
+var name = age >= 18 ? 'Cenab' : 'Balaca';
+
+console.log('name', name);
