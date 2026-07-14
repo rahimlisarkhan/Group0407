57b35b5ad6d636ef5e56b66a001b126c0f083c3d
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-07-14 21:50:31 +0400

    upt

--- Stage2/Topic1,2,3/callback.js
+++ Stage2/Topic1,2,3/callback.js
@@ -132,26 +132,188 @@
 // console.log(ustaninCavabi2);
 // console.log(ustaninCavabi3);
 
-function a1(num1, num2) {
-  // logic....
+// function a1(num1, num2) {
+//   // logic....
+// }
+
+// function a2(num1, num2) {
+//   // logic....
+// }
+
+// function a3(num1, num2) {
+//   // logic....
+// }
+
+// function A(n1, n2, callback) {
+//   var total = n1 + n2;
+
+//   var result = callback(total, 100);
+
+//   return result;
+// }
+
+// var netice1 = A(45, 21, a1);
+// A(45, 21, a2);
+// A(45, 21, a3);
+
+// function A() {
+//   var num = 4;
+
+//   console.log(num);
+// }
+
+// var A = function () {
+//   var num = 4;
+
+//   return num;
+// };
+
+// console.log(A); // function () {
+
+//   var num = 4;
+
+//   console.log(num);
+// };
+
+// var LIST = [1, 2, 3, 4];
+
+// console.log(LIST); // [1, 2, 3, 4]
+
+// var result = A(); //undefined
+
+// console.log(result); // 4
+
+// var b = 5;
+
+// console.log(b);
+
+// b = 'Hello';
+
+function B() {
+  console.log('B working...');
 }
 
-function a2(num1, num2) {
-  // logic....
+function C() {
+  console.log('C working...');
 }
 
-function a3(num1, num2) {
-  // logic....
+// function A(cagirilacaqFn) {
+//   cagirilacaqFn();
+
+//   console.log('A working...');
+// }
+
+// function A1() {
+//   C();
+
+//   console.log('A working...');
+// }
+
+// function A2() {
+//   B();
+
+//   console.log('A working...');
+// }
+
+// A1();
+// A2();
+
+// A(C);
+// A(B);
+
+// var list = [1, 2, 3, 4];
+// var list2 = ['a', 'b', 'c', 'd'];
+
+// for (let i = 0; i < list.length; i++) {
+//   const element = list[i];
+
+//   console.log(element);
+// }
+
+// for (let i = 0; i < list2.length; i++) {
+//   const element = list2[i];
+
+//   console.log(element);
+// }
+
+// function render(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+
+//     console.log(element);
+//   }
+// }
+
+// render(list);
+// render(list2);
+
+var list = [1, 2, 3, 4, 5, 6];
+
+function handleElVurma(element) {
+  var yeniNetice = element * 3;
+
+  return yeniNetice;
 }
 
-function A(n1, n2, callback) {
-  var total = n1 + n2;
+// function handleElToplama(element) {
+//   var yeniNetice = element + 3;
 
-  var result = callback(total, 100);
+//   return yeniNetice;
+// }
 
-  return result;
+// function handleElVariantAdlari(element) {
+//   var yeniNetice = `Variant ${element}`;
+
+//   return yeniNetice;
+// }
+
+function renderMap(arr, isGoren) {
+  var newArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    var el = arr[i];
+
+    // var result = el * num;
+    var result = isGoren(el);
+
+    newArr.push(result);
+  }
+
+  return newArr;
 }
 
-var netice1 = A(45, 21, a1);
-A(45, 21, a2);
-A(45, 21, a3);
+var netice = renderMap(list, handleElVurma);
+
+var netice2 = renderMap(list, function (element) {
+  var yeniNetice = `Variant ${element}`;
+
+  return yeniNetice;
+});
+
+var netice3 = renderMap(list, function (element) {
+  var yeniNetice = element + 3;
+
+  return yeniNetice;
+});
+
+console.log(netice);
+console.log(netice2);
+console.log(netice3);
+
+// function renderMap(arr, num) {
+//   var newArr = [];
+
+//   for (var i = 0; i < arr.length; i++) {
+//     var el = arr[i];
+
+//     var result = el * num;
+
+//     newArr.push(result);
+//   }
+
+//   return newArr;
+// }
+
+// var netice = renderMap(list, 3);
+
+// console.log(netice);
