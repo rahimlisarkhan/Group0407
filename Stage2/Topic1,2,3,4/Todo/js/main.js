db5a482af339d29c1878beaa1ef84bf7e897e482
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-08-15 11:35:07 +0400

    upt

---
+++ Stage2/Topic1,2,3,4/Todo/js/main.js
@@ -0,0 +1,56 @@
+//FIND
+const todoInput = document.querySelector('#todo-input');
+const saveBtn = document.querySelector('#save-btn');
+const todoList = document.querySelector('#todo-list');
+const alertCheck = document.querySelector('#alertCheck');
+
+const data = []; // DATA LOGIC
+
+saveBtn.addEventListener('click', function () {
+  const todoValue = todoInput.value; // str
+
+  // VALIDATION
+  if (todoValue.trim() == '') {
+    // alert('Zehmet olmasa duzgun doldurun');
+
+    // CHANGE
+    alertCheck.classList.remove('d-none');
+    return;
+  }
+
+  // CHANGE
+  alertCheck.classList.add('d-none');
+
+  // arrayi icine yigaq
+  data.push(todoValue);
+
+  // POST - data -> server app
+
+  renderList();
+
+  todoInput.value = ''; //reset value
+});
+
+function renderList() {
+  const newData = data
+    .map((el) => {
+      return `<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                  <span>${el}</span>
+                </li>
+            `;
+    })
+    .join('');
+
+  // CHANGE
+  todoList.innerHTML = newData;
+}
+
+const numbers = [2, 4, 6, 8, 10];
+
+const numbers2 = numbers.map((n) => {
+  return `Salam: ${n * 4}`;
+});
+
+// todoList.innerHTML = numbers2.join('');
+
+// console.log('numbers2', numbers2);
--- Stage2/Topic1,2,3,4/dom.js
+++ Stage2/Topic1,2,3,4/dom.js
@@ -4,13 +4,48 @@
 
 // FIND
 
-const titleEl = document.querySelector('h1');
+// const titleEl = document.querySelector('h1');
 
-console.log(titleEl);
+// console.log(titleEl);
 
-// CHANGE & CREATE
-titleEl.innerHTML = 'Salam';
-titleEl.style.backgroundColor = 'red';
-titleEl.setAttribute('class', 'text-danger');
+// // CHANGE & CREATE
+// titleEl.innerHTML = 'Salam';
+// // titleEl.style.backgroundColor = 'red';
+// // titleEl.setAttribute('class', 'text-danger');
 
-// REMOVE
+// // REMOVE
+
+//FIND
+// const listContent = document.querySelector('#listContent');
+// const pEl = document.querySelector('#para');
+
+// console.log('listContent', listContent);
+
+// const imgTag = document.createElement('img');
+
+// imgTag.src =
+//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKrZ8_T1gZyIsTKl4XBZmtZ6GYMb9UNNSf3kF3uBxHdWwi7hxNlcUPTFE&s=10';
+
+// const result = imgTag.classList.contains('img-fluid');
+
+// const el = listContent.nextElementSibling;
+
+// el.innerHTML = 'Salam';
+
+// listContent.style.backgroundColor = 'yellow';
+// listContent.append(imgTag);
+
+// listContent.removeChild(pEl);
+
+const inputfullName = document.querySelector('#fullName');
+const btnSend = document.querySelector('#btnSend');
+
+btnSend.addEventListener('click', function () {
+  const inputValue = inputfullName.value;
+
+  console.log('inputValue', inputValue);
+
+  // CHANGE - STYLE, ELEMENT or Content, Attr, Class, Check logic
+  inputfullName.style.border = '2px solid green';
+  inputfullName.value = '';
+});
