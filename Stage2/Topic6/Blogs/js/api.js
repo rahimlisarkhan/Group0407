8cfe0c61adb541ab162cda66e7367f4c4fa9ab71
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-09-11 22:00:42 +0400

    updated

--- Stage2/Topic6/Blogs/js/api.js
+++ Stage2/Topic6/Blogs/js/api.js
@@ -10,13 +10,14 @@ async function getBlogs() {
 
   const data = await response.json();
 
-  return data;
+  return data.reverse();
 }
 
-async function getBlogById(id) {
-  const url = `${baseURL}/posts/${id}?_embed=comments`;
+async function getBlogById(id, commentActive = true) {
+  const url = `${baseURL}/posts/${id}`;
+  const urlComment = `${baseURL}/posts/${id}?_embed=comments`;
 
-  const response = await fetch(url, {
+  const response = await fetch(commentActive ? urlComment : url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -25,7 +26,62 @@ async function getBlogById(id) {
 
   const data = await response.json();
 
-  console.log('data', data);
+  return data;
+}
+
+async function crtBlog(payload) {
+  const response = await fetch(baseURL + '/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) throw new Error('Blog could not be created!');
+
+  const data = await response.json();
 
   return data;
 }
+
+async function uptBlog(id, payload) {
+  const url = baseURL + '/posts/' + id;
+
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) throw new Error('Blog could not be updated!');
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function rmvBlog(id) {
+  const url = baseURL + '/posts/' + id;
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) throw new Error('Blog could not be deleted!');
+
+  const data = await response.json();
+
+  return data;
+}
+
+// const testData = {
+//   title: 'Yeni ders elvin',
+//   body: 'Lorem ipsum bla bla',
+// };
+
+// crtBlog(testData);
--- Stage2/Topic6/Blogs/js/detail.js
+++ Stage2/Topic6/Blogs/js/detail.js
@@ -5,13 +5,24 @@ const titleEl = document.getElementById('blog-title');
 const idEl = document.getElementById('blog-id');
 const bodyEl = document.getElementById('blog-body');
 const commentsListEl = document.getElementById('comments-list');
+const removeBtn = document.querySelector('#blog-remove');
+const editBtn = document.querySelector('#blog-edit');
+const removeSpinner = document.getElementById('remove-spinner');
+const removeText = document.getElementById('remove-text');
 
 function renderBlogDetail(blog) {
   titleEl.textContent = blog.title;
   idEl.textContent = `#${blog.id}`;
   bodyEl.textContent = blog.body;
-
+  editBtn.href = `./create.html?id=${blog.id}`;
   detailEl.classList.remove('d-none');
+}
+
+function generateId() {
+  const params = new URLSearchParams(window.location.search);
+  const dynamicId = params.get('id');
+
+  return dynamicId;
 }
 
 function renderComments(comments = []) {
@@ -40,11 +51,46 @@ function renderComments(comments = []) {
   });
 }
 
+function setRemoveLoading(isLoading) {
+  removeBtn.disabled = isLoading;
+  removeSpinner.classList.toggle('d-none', !isLoading);
+  removeText.textContent = isLoading ? 'Deleting...' : 'Delete';
+
+  // card soluq gorunur ki, silindiyi hiss olunsun
+  detailEl.classList.toggle('opacity-50', isLoading);
+}
+
+removeBtn.addEventListener('click', async function () {
+  const dynamicId = generateId();
+
+  try {
+    // loading true
+    setRemoveLoading(true);
+
+    await rmvBlog(dynamicId);
+
+    // Success Alert logic
+    alertEl.textContent = 'Blog deleted successfully! Redirecting...';
+    alertEl.className = 'alert alert-success';
+
+    setTimeout(() => {
+      window.location.href = '../index.html';
+    }, 2000);
+  } catch (err) {
+    // Error Alert logic
+    alertEl.textContent = 'Blog could not be deleted!';
+    alertEl.className = 'alert alert-danger';
+    console.log(err);
+
+    // loading false (yalniz xeta olanda, ugurlu halda redirect-e qeder button disabled qalir)
+    setRemoveLoading(false);
+  }
+});
+
 async function startApp() {
   try {
     // catch id from html (url query param)
-    const params = new URLSearchParams(window.location.search);
-    const dynamicId = params.get('id');
+    const dynamicId = generateId();
 
     if (!dynamicId) throw new Error('Blog id not found!');
 
